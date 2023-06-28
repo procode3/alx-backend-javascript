ceec5d1905f00399f37276aa1249c7a07c780583
procode3 <wr.t8r1@gmail.com>
2023-06-28 10:31:18 -0700

    Try catch in JS

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+export default function divideFunction(numerator, denominator) {
+   try {
+     const out = numerator / denominator;
+     if (denominator === 0) {
+       throw new Error()
+     }
+     return out;
+   } catch (err) {
+     throw Error('cannot divide by 0');
+
+}
+}
