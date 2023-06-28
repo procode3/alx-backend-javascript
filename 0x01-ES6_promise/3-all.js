a56b2802dd9c73be6095baa580d5c425a81c086d
procode3 <wr.t8r1@gmail.com>
2023-06-28 03:54:14 -0700

    Primise.all implementation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
     .then((data) => {
-      const [photoOut, user] = data;
-      console.log(`${photoOut.body} ${user.firstName} ${user.lastName}`);
+      const [user, photoOut] = data;
+      console.log(photoOut.body, user.firstName, user.lastName);
     })
     .catch(() => {
       console.Error('Signup system offline');
