43e717a4d069110019a74df50f7fe81c407dfa71
procode3 <wr.t8r1@gmail.com>
2023-06-28 04:04:03 -0700

    Primise.all implementation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const data1 = uploadPhoto();
-  const data2 = createUser();
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
