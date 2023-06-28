d8523677d1f61bdf3f168c8344cb120251693fed
procode3 <wr.t8r1@gmail.com>
2023-06-28 01:06:01 -0700

    Updated promise to a func

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,10 @@
-export default new Promise();
+export default function promise(test) {
+  return new Promise((resolve, reject) => {
+    if (test) {
+      resolve(true);
+    }
+    if (!test) {
+      reject(Error('Failed'));
+    }
+  });
+}
