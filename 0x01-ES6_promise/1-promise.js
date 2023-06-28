8b8202e289472edd0249dc3015b152b7933d0ba2
procode3 <wr.t8r1@gmail.com>
2023-06-28 01:18:19 -0700

    Updating error object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if(success) {
-      resolve({status:200, body:'sucess',})
+    if (success) {
+      resolve({ status: 200, body: 'sucess' });
+    } else {
+      reject(new Error({ Error: 'The fake API is not working currently' }));
     }
-    reject(throw new Error('The fake API is not working currently') )
   });
-} 
+}
