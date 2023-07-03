4c839b336e10c30c11930074ca15fdcb3e4aa4aa
procode3 <wr.t8r1@gmail.com>
2023-07-03 13:58:29 -0700

    Updating values in a map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(myMap) {
+  try {
+    const list = Array.from(myMap.keys());
+
+    return (list.filter((item) => myMap.get(item) === 1).map((item) => myMap.set(item, 100)));
+  } catch (err) {
+    throw new Error(err);
+  }
+}
