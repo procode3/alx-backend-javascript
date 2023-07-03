b27ef7d470b2fa5a3a224b734ffdbd337417cfc4
procode3 <wr.t8r1@gmail.com>
2023-07-03 13:23:42 -0700

    a function that returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(mySet, str) {
-  if (!str) {
+  if (str.length === 0) {
     return '';
   }
   return Array.from(mySet).filter((item) => item.startsWith(str)).map((item) => item.slice(str.length)).join('-');
