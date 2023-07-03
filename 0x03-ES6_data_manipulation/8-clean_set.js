96513fb2e7f0c9c0252fb3954fdc4dfb0aee8dc1
procode3 <wr.t8r1@gmail.com>
2023-07-03 14:03:33 -0700

    Updating substring checks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (startString.length === 0) {
     return '';
   }
   return Array.from(set).filter((item) => item.startsWith(startString)).map((item) => item.slice(startString.length)).join('-');
