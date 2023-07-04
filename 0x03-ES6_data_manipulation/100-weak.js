5dd795588b9a48f3a1b1bd5bbc0c59ccb177dcf1
procode3 <wr.t8r1@gmail.com>
2023-07-04 01:40:29 -0700

    Weakmap implementaion

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,8 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-    return;
+    weakMap.set(endpoint, 0);
   }
   if (weakMap.get(endpoint) < 5) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
