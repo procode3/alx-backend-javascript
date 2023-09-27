292432b61b2f8f2557a865a901b17de51b7c5a26
Erickb <wr.t8r1@gmai.com>
2023-09-27 00:03:54 -0700

    Running test with Sinon

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,12 +5,16 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber with type SUM', () => {
+    // Create a spy for the Utils.calculateNumber function
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
+    // Call the function to be tested
     sendPaymentRequestToApi(100, 20);
-   
-    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
 
+    // Assert that the spy was called with the correct arguments
+    assert(calculateNumberSpy.calledWithExactly('SUM', 100, 20));
+
+    // Restore the spy to its original state to prevent side effects in other tests
     calculateNumberSpy.restore();
   });
 });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,5 +1,6 @@
 const Utils = {
     calculateNumber: (type, a, b) => {
+      // Your code for calculating the sum or other operations goes here
       if (type === 'SUM') {
         return a + b;
       }
