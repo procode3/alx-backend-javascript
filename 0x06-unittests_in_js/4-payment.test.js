697a84c54ded0e0a35d0294199366193fab7f780
Erickb <wr.t8r1@gmai.com>
2023-09-27 00:08:01 -0700

    Running stub test with Sinon

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+// 4-payment.test.js
+
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber with type SUM', () => {
+    // Create a spy for console.log to verify its output
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    // Create a stub for Utils.calculateNumber to always return 10
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    // Call the function to be tested
+    sendPaymentRequestToApi(100, 20);
+
+    // Assert that the stub was called with the correct arguments
+    assert(calculateNumberStub.calledWithExactly('SUM', 100, 20));
+
+    // Assert that the console.log spy was called with the correct message
+    assert(consoleLogSpy.calledWithExactly('The total is: 10'));
+
+    // Restore the stub and spy to their original states
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
